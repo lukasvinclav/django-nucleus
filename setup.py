9120349493653dc2a48d81ef2bd534f619acd405
Lukas <lukas.vinclav@gmail.com>
2019-03-15 19:22:26 +0100

    tables improvements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     short_description='Theme for Django admin',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/lukasvinclav/django-nucleus',
     packages=[
         'nucleus',
